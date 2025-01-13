8d06bc0d48223db832ce1e619109a4b53f3cf2e3
Bernd-Uwe Rogalla <b-u.rogalla@ostfalia.de>
2025-01-13 11:42:28 +0100

    initial

--- app/src/main/java/de/rogallab/mobile/ui/features/news/NewsIntent.kt
+++ app/src/main/java/de/rogallab/mobile/ui/features/news/NewsIntent.kt
@@ -1,7 +1,5 @@
 package de.rogallab.mobile.ui.features.news
 
-import de.rogallab.mobile.data.dtos.Article
-
 sealed class NewsIntent {
    data class  SearchTextChange(val searchText: String) : NewsIntent()
    data object TriggerSearch : NewsIntent()
